13f43687c0e64d9c707c9a32ec2b607c169be57b
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-04-21 16:40:13 +0900

    rename & 調整

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,14 +3,14 @@ class HomesController < ApplicationController
   def index
     @arr = []
     @@timetables.each do |timetable|
-      @time_arr = []
-      @time_arr << timetable.class_time
-      @time_arr << timetable.class_name
-      @time_arr << timetable.class_room
-      @time_arr << timetable.professor_name
-      @arr << @time_arr
+      @class_arr = []
+      @class_arr << timetable.class_time
+      @class_arr << timetable.class_name
+      @class_arr << timetable.class_room
+      @class_arr << timetable.professor_name
+      @arr << @class_arr
     end
-    # 配列をJsonへ変換する
+    # array to Json
     @arr_json = @arr.to_json.html_safe
   end
 
@@ -18,8 +18,6 @@ class HomesController < ApplicationController
 
   def user_has_timetable?
     @@timetables = current_user.school_timetables
-    if @@timetables.empty?
-      redirect_to edit_user_path(current_user)
-    end
+    # redirect_to edit_user_path(current_user) if @@timetables.empty?
   end
 end
