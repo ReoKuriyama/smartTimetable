d16106d8142583624d3b682c9c3b73813192c159
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-04-25 15:29:45 +0900

    refactoring
    jsの記述をいじれば普通に配列、ハッシュ混合のものを受け取れた

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,23 +1,17 @@
 class HomesController < ApplicationController
+  attr_accessor :timetables
   before_action :user_has_timetable?
   def index
-    @arr = []
-    @@timetables.each do |timetable|
-      @class_arr = []
-      @class_arr << timetable.class_time
-      @class_arr << timetable.class_name
-      @class_arr << timetable.class_room
-      @class_arr << timetable.professor_name
-      @arr << @class_arr
-    end
-    # array to Json
-    @arr_json = @arr.to_json.html_safe
+    @arr_json = @timetables.to_json.html_safe
   end
 
   private
 
   def user_has_timetable?
-    @@timetables = current_user.school_timetables
+    @timetables = {}
+    c_user_tt = current_user.school_timetables
+    @timetables[:spring] = c_user_tt.where(class_type: 0)
+    @timetables[:autumn] = c_user_tt.where(class_type: 1)
     # redirect_to edit_user_path(current_user) if @@timetables.empty?
   end
 end
