dde5a10e2ebd1629ea76311780f6c198433304c6
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-04-25 18:29:48 +0900

    refactoring
    時間割を保持しているかどうかの判別方法の変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,16 +2,18 @@ class HomesController < ApplicationController
   attr_accessor :timetables
   before_action :user_has_timetable?
   def index
+    @timetables = {}
+    c_user_tt = current_user.school_timetables
+    @timetables[:spring] = c_user_tt.where(class_type: 0)
+    @timetables[:autumn] = c_user_tt.where(class_type: 1)
     @arr_json = @timetables.to_json.html_safe
   end
 
   private
 
   def user_has_timetable?
-    @timetables = {}
-    c_user_tt = current_user.school_timetables
-    @timetables[:spring] = c_user_tt.where(class_type: 0)
-    @timetables[:autumn] = c_user_tt.where(class_type: 1)
-    # redirect_to edit_user_path(current_user) if @@timetables.empty?
+    timetables = current_user.taking_classes.first
+    binding.pry
+    redirect_to edit_user_path(current_user) if timetables.empty?
   end
 end
