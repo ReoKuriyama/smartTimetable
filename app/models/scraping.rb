26bf2294b2505aa813c7f2198657d2cb850b1903
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-05-06 14:09:39 +0900

    リファクタリング
    ハッシュでスクレイピング実行時に必要な情報を受け取る
    sessionは他大学でも共通なためメソッド化
    
    大学ごとに条件分岐ができるよう実装

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -2,46 +2,56 @@ require 'nokogiri'
 require 'capybara'
 require 'capybara/poltergeist'
 
-# session.save_screenshot 'screenshot.png'
-
 class Scraping
-  def self.get_classes(email, password, user_id)
-    # poltergist setting
+  attr_reader :email, :password, :c_user_id, :univ_name, :session
+  def initialize(args)
+    @email = args[:email]
+    @password = args[:password]
+    @c_user_id = args[:c_user_id]
+    @univ_name = args[:univ_name]
+    @session = create_session
+  end
+
+  def create_session
     Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, {:js_errors => false, :timeout => 1000 })
+      Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 1000)
     end
     Capybara.default_selector = :xpath
     session = Capybara::Session.new(:poltergeist)
-
     session.driver.headers =
       { 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X)' }
-    # keio.jp
+    session
+  end
+
+  def scrape_timetables
+    case univ_name
+    when 'keio' then
+      keio_scraping
+    else
+      puts 'uncorrect univ name'
+      session.driver.quit
+      false
+    end
+  end
+
+  def keio_scraping
     session.visit 'https://auth.keio.jp/'
     # login
     session.find("//*[@id='username']").send_keys(email)
     session.find("//*[@id='password']").send_keys(password)
     # click login button
     session.find("//*[@id='login']/section[5]/button").click
-
     # when login failed
     if session.current_url != 'https://portal.keio.jp/koid/'
       session.driver.quit
-      return false
+      false
     end
-
     # click button
     session.find("//*[@id='b_menu']/li[2]").click
     session.find("//*[@id='b_menu']/li[2]/ul/li[1]").click
-
-    # switch tab
-    # session.switch_to_window(session.windows.last)
-
     sleep(2)
-
     session.visit 'https://gslbs.adst.keio.ac.jp/student/schedule/View_Schedule.php'
-
     sleep(2)
-
     html = session.html
     doc = Nokogiri::HTML.parse(html)
 
@@ -49,12 +59,12 @@ class Scraping
     spring_subjects = doc.search('/html/body/center/table[2]').search('.calendar')
     autumn_subjects = doc.search('/html/body/center/table[3]').search('.calendar')
 
-    save_classes(spring_subjects, 0, user_id)
-    save_classes(autumn_subjects, 1, user_id)
+    save_classes(spring_subjects, 0)
+    save_classes(autumn_subjects, 1)
     session.driver.quit
   end
 
-  def self.save_classes(subjects, type, user_id)
+  def save_classes(subjects, type)
     # カラム名生成のため変数
     n = 11
     s = 1
@@ -73,11 +83,11 @@ class Scraping
             class_room: subject.search('.room').inner_text.gsub('教室', ''),
             professor_name: array[2],
             class_type: type,
-            user_ids: [user_id]
+            user_ids: [c_user_id]
           )
         else
           TakingClass.create(
-            user_id: user_id,
+            user_id: c_user_id,
             school_timetable_id: timetable[0].id
           )
         end
