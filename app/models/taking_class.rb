96df89eef8a15e05d99836522846589b2a0b3c18
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-05-06 15:15:53 +0900

    taking_classに複数ユニーク設定

--- app/models/taking_class.rb
+++ app/models/taking_class.rb
@@ -1,4 +1,7 @@
 class TakingClass < ApplicationRecord
   belongs_to :user
   belongs_to :school_timetable
+
+  # validation
+  validates :user_id, uniqueness: { scope: %i[school_timetable_id] }
 end
