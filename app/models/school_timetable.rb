2fe2eb769f5b7cd3fd14a944e4bf577d092f6034
Reo Kuriyama <gagabornthisway34@gmail.com>
2018-06-09 18:17:33 +0900

    Refactor validate option

--- app/models/school_timetable.rb
+++ app/models/school_timetable.rb
@@ -1,6 +1,10 @@
 class SchoolTimetable < ApplicationRecord
+  # association
   has_many :taking_classes
   has_many :users, through: :taking_classes
+
+  # validation
+  validates :class_time, :class_name, :class_room, :professor_name, :class_type, presence: true
 
   enum class_type: { spring: 0, autumn: 1 }
 end
